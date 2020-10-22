179118d0e51ace4fb7d688cc85622bad47261316
hanielwang <nosleep2008@ucdconnect.ie>
2020-10-23 00:09:45 +0100

    init

---
+++ src/main/java/com/limitedtimeoffers/sprintboot/SprintbootApplication.java
@@ -0,0 +1,15 @@
+package com.limitedtimeoffers.sprintboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprintbootApplication.class, args);
+    }
+
+
+
+}
